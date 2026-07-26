cc235ca6c852e47da03c0cf8a42c9b3aea7903f2
Khaled Mazahreh <khaledmazahreh@Khaleds-MacBook-Air.local>
2026-07-26 14:30:44 +0300

    Finalise query (v3), pull 12 months of Guardian headlines (298 articles, 53 weekly JSON files), change GDELT API to Guardian API for better rate limiting

--- src/ingest.py
+++ src/ingest.py
@@ -1,60 +1,92 @@
-from datetime import datetime
-from urllib.parse import quote
+from datetime import datetime, timedelta
+import os
 import pathlib
 import requests
 import time 
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dotenv import load_dotenv 
 
-from matplotlib.dates import relativedelta  
+load_dotenv()
 
+API_KEY = os.getenv("GUARDIAN_API_KEY")
 DIRECTORY_PATH = pathlib.Path(__file__).resolve().parents[1] / "data" / "raw"
-THROTTLE_MESSAGE = "Please limit requests"
+BASE_URL = "https://content.guardianapis.com/search"
 
-def build_url(query: str, start: datetime, end: datetime, maxrecords: int = 250) -> str:
-    start_gdelt_time = start.strftime("%Y%m%d%H%M%S")
-    end_gdelt_time = end.strftime("%Y%m%d%H%M%S")
-    encoded_query = quote(query)
+def build_guardian_params(query: str, start: datetime, end: datetime, page_size: int = 50) -> dict:
+    start_time = start.strftime("%Y-%m-%d")
+    end_time = end.strftime("%Y-%m-%d")
 
     #Construct the final API endpoint URL
-    base_url ="https://api.gdeltproject.org/api/v2/doc/doc?"
-    url = f"{base_url}query={encoded_query}&startdatetime={start_gdelt_time}&enddatetime={end_gdelt_time}&maxrecords={maxrecords}&format=json&mode=artlist"
+    params = {
+        "q": query,
+        "query-fields": "headline",
+        "from-date": start_time,
+        "to-date": end_time,
+        "page-size": page_size,
+        "api-key": API_KEY  
+    }
 
-    return url
+    return params
 
-def fetch_window(url: str) -> list:
+def fetch_window(params: dict) -> list:
     """
-    Fetches articles from the GDELT API for a given time window and appends them to the article_list.
+    Fetches articles from the Guardian API for a given time window and appends them to the article_list.
     """
     for attempt in range(3):
-        response = requests.get(url, timeout = 30)
+        response = requests.get(BASE_URL, params=params, timeout = 30)
 
-        if response.text.lstrip().startswith(THROTTLE_MESSAGE):
+        if response.status_code not in (200, 429):  # Check for successful response or rate limiting   
+            print(f"Error fetching data: {response.status_code} - {response.text}")
+            return []  # Return an empty list if there's an error other than rate limiting
+
+        if response.status_code == 429:  # Check for rate limiting
             print(f"Request throttled. Attempt {attempt + 1} of 3. Retrying in 30 seconds...")
             time.sleep(30)  # Wait for 30 seconds before retrying
             continue  # Retry the request
         
         try:
             data = response.json()
-            articles = data.get("articles", [])
+            articles = data["response"]["results"]
 
-        except ValueError as e:
+        except (ValueError, KeyError) as e:
             print(f"Error parsing JSON response: {e}")
             return []  # Return an empty list even if there's a parsing error
         
         return articles  # Exit the loop if the request was successful
     return []  # Return an empty list if all attempts fail
 
-   
+def fetch_range(query: str, start: datetime, end: datetime, page_size: int = 50) -> None:
+    """
+    Fetches articles from the Guardian API week by week.
+    """
+    if not DIRECTORY_PATH.exists():
+            DIRECTORY_PATH.mkdir(parents=True, exist_ok=True)
+    current = start 
 
+    while current < end:
+        end_of_week = current + timedelta(days=7) 
+        filename = f"articles_{current.strftime('%Y%m%d')}_{end_of_week.strftime('%Y%m%d')}.json"
+        #Skip to the next week if file already exists
+        if (DIRECTORY_PATH / filename).exists():
+            print(f"File {filename} already exists. Skipping fetch.")
 
+        else:
+            params = build_guardian_params(query, current, end_of_week, page_size)
+            list_of_articles = fetch_window(params)
+            print(f"Fetched {len(list_of_articles)} articles for the week starting {current.strftime('%Y-%m-%d')} to {end_of_week.strftime('%Y-%m-%d')}.")
 
+            #Create the directory if it doesn't exist
+            # Save the articles to a JSON file
+            with open(DIRECTORY_PATH / filename, "w") as file:
+                json.dump(list_of_articles, file, indent=4)
+            time.sleep(1)  # Sleep for 1 second to avoid hitting the API too quickly
+
+        current = end_of_week 
 
 
 
 if __name__ == "__main__":
-    query = '(heatwave OR heatwaves OR "dust storm" OR hurricane OR "Polar Vortex" OR drought OR Dunkelflaute OR snowfall) ("power grid" OR "electricity grid" OR "blackout" OR "power outage" OR "load shedding" OR "energy demand" OR "electricity demand" OR "peak demand" OR "grid reliability" OR "power failure" OR "power plant" OR "substation" OR "transmission lines" OR "hydroelectric dam" OR "nuclear plant" OR "wind farm" OR "solar array" OR "solar panels" OR  "natural gas" OR "heating oil" OR "electricity prices" OR "energy prices" OR "power supply" OR "generation shortfall")  (Germany OR France OR Spain OR Netherlands OR "United Kingdom") sourcelang:eng'
-    start_date = datetime.now() - relativedelta(months=6)
-    end_date = datetime.now()
-    url = build_url(query, start_date, end_date)
-    list_of_articles = fetch_window(url)
-    print(url)
-    print(f"Number of articles fetched: {len(list_of_articles)}")
+    query = '(electricity OR "power prices" OR "energy prices" OR blackout OR "power outage" OR "power cut" OR "National Grid" OR "gas supply" OR "wind" OR "solar" OR "energy crisis" OR "power grid" OR "wind power" OR "solar power" OR "energy bills")'
+    fetch_range(query, datetime.now() - relativedelta(months=12), datetime.now(), page_size=100)
